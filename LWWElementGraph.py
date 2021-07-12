397abae61f1f3759046e63a75ce745ea67aa6b13
krohak <rohaksinghal14@gmail.com>
2021-07-12 10:32:05 +0800

    refactor

--- LWWElementGraph.py
+++ LWWElementGraph.py
@@ -9,18 +9,21 @@ class LWWElementGraph(object):
         self.edges = LWWElementSet()
         # to optimize getNeighborsOf and findPath, 
         # maintaining a live updating internal state
-        self.graphState = defaultdict(set)
+        self.graphState = defaultdict(list)
 
     def __repr__(self):
         return "Graph: \n{} \nHistory: \nVertices: \n{}\n Edges: \n{}".format(self.graphState, self.vertices, self.edges) 
 
     def addVertex(self, vertex):
-        ''' '''
+        ''' Adds the Vertex to vertices LWWSet. Also maintains graphState for read optimization
+            Runs in O(1)'''
         self.vertices.addElement(vertex)
         self.graphState[hashObj(vertex)]
 
     def removeVertex(self, vertex):
-        ''' '''
+        ''' If vertex is present, then add it to vertices.removeSet. Add each of its edge to 
+            edges.removeSet. Also maintains graphState for read optimization.
+            Runs in O(E) because of _removeVertex '''
         if not self.vertices.isMember(vertex):
             raise KeyError("Vertex {} not in LWWElementGraph".format(vertex))
         self.vertices.removeElement(vertex)
@@ -29,7 +32,8 @@ class LWWElementGraph(object):
         self.graphState = self._removeVertex(self.graphState, vertex)
         
     def addEdge(self, vertex1, vertex2):
-        ''' '''
+        ''' If vertex1, vertex2 present, add edge to edges.addSet. Maintain graphState 
+            Runs in O(1) '''
         if not self.vertices.isMember(vertex1):
             raise KeyError("Vertex {} not in LWWElementGraph".format(vertex1))
         elif not self.vertices.isMember(vertex2):
@@ -38,7 +42,8 @@ class LWWElementGraph(object):
         self.graphState = self._addEdge(self.graphState, vertex1, vertex2)
 
     def removeEdge(self, vertex1, vertex2):
-        ''' '''
+        ''' If edge present, add it to edges.removeSet. Maintain graphState.
+            Runs in O(E) because of _removeEdge'''
         edgeSet = {vertex1, vertex2}
         if not self.edges.isMember(edgeSet):
             raise KeyError("Edge {}-{} not in LWWElementGraph".format(vertex1, vertex2))
@@ -46,21 +51,24 @@ class LWWElementGraph(object):
         self.graphState = self._removeEdge(self.graphState, vertex1, vertex2)
 
     def isMember(self, vertex):
-        ''' '''
+        ''' Check if vertex is valid, runs in O(1) '''
         return hashObj(vertex) in self.graphState
 
     def getNeighborsOf(self, vertex):
-        ''' '''
+        ''' O(1) query for all the vertices connected to the query vertex. Uses graphState 
+            which was optimized for read '''
         if not self.isMember(vertex):
             raise KeyError("Vertex {} not in LWWElementGraph".format(vertex))
         return self.graphState[hashObj(vertex)]
 
     def findPath(self, vertex1, vertex2):
-        ''' Perform BFS '''
+        ''' Perform BFS for shortest path. Uses graphState which was optimized for read
+            to get all the neighbours of a vertex in O(1).
+            Runs in O(V + E) '''
         frontier, explored, ancestory = [vertex1], set(), {}
         while frontier:
             node = frontier.pop(0)
-            explored.add(node)
+            explored.add(hashObj(node))
             if node == vertex2:
                 path = [node]
                 while hashObj(node) in ancestory:                     
@@ -68,14 +76,14 @@ class LWWElementGraph(object):
                     path.append(node) 
                 return path[::-1]
             for ngbr in self.getNeighborsOf(node):
-                if ngbr not in frontier and ngbr not in explored:
+                if ngbr not in frontier and hashObj(ngbr) not in explored:
                     frontier.append(ngbr)
                     ancestory[hashObj(ngbr)] = node
         return []
 
     def mergeGraphs(self, otherGraph):
         ''' Merging Graphs by merging their Vertice and Edge LLWSet. Remove Edge if Vertex not present
-            anymore after merge. Recompute the internal graphState as well. Runs in O(E + V)'''
+            anymore after merge. Recompute the internal graphState as well. Runs in O(V + E)'''
         self.vertices.mergeWith(otherGraph.vertices)
         self.edges.mergeWith(otherGraph.edges)
         for v1, v2 in self.edges.getMembers():
@@ -84,27 +92,27 @@ class LWWElementGraph(object):
         self.graphState = self._computeGraph(self.vertices.getMembers(), self.edges.getMembers())
 
     def _removeVertex(self, graphState, vertex):
-        ''' '''
+        ''' Runs in O(E) '''
         del graphState[hashObj(vertex)]
-        for k in graphState.keys(): graphState[k].discard(vertex)
+        for k in graphState.keys(): graphState[k].remove(vertex) if vertex in graphState[k] else None
         return graphState
 
     def _addEdge(self, graphState, vertex1, vertex2):
-        ''' '''
-        graphState[hashObj(vertex1)].add(vertex2)
-        graphState[hashObj(vertex2)].add(vertex1)
+        '''  Runs in O(1) '''
+        graphState[hashObj(vertex1)].append(vertex2)
+        graphState[hashObj(vertex2)].append(vertex1)
         return graphState
 
     def _removeEdge(self, graphState, vertex1, vertex2):
-        ''' '''
+        '''  Runs in O(E) '''
         graphState[hashObj(vertex1)].remove(vertex2)
         graphState[hashObj(vertex2)].remove(vertex1)
         return graphState
 
     def _computeGraph(self, vertices, edges):
         ''' Calculate the graphState using latest vertices and edges. Initialize the Vertices first, 
-            since there be some vertices with no edges'''
-        graphState = defaultdict(set)
+            since there be some vertices with no edges. Runs in O(V + E)'''
+        graphState = defaultdict(list)
         for v in vertices: graphState[hashObj(v)]
-        for a,b in edges: graphState[hashObj(a)].add(b); graphState[hashObj(b)].add(a)
+        for a,b in edges: graphState[hashObj(a)].append(b); graphState[hashObj(b)].append(a)
         return graphState
--- tests/testLWWElementGraph.py
+++ tests/testLWWElementGraph.py
@@ -17,14 +17,14 @@ def createComplexObj():
 class LWWElementGraphTests(TestCase):
 
     def testInit(self):
-        ''' '''
+        ''' Check if Vertices and Edges are initialized to LWWElementSet instance '''
         g = LWWElementGraph()
         self.assertTrue(isinstance(g.vertices, LWWElementSet))
         self.assertTrue(isinstance(g.edges, LWWElementSet))
         
     @mock.patch('LWWElementSet.LWWElementSet.addElement')
     def testAddVertex(self, mockSetAddElement):
-        ''' '''
+        ''' Check if vertices.addElement is called '''
         g = LWWElementGraph()
         g.graphState = mock.MagicMock()
         g.addVertex('a')
@@ -33,7 +33,7 @@ class LWWElementGraphTests(TestCase):
 
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     def testRemoveFails(self, mockIsMember):
-        ''' '''
+        ''' Cannot remove before adding vertex '''
         mockIsMember.return_value = False
         g = LWWElementGraph()
         with self.assertRaises(KeyError):
@@ -44,7 +44,7 @@ class LWWElementGraphTests(TestCase):
     @mock.patch('LWWElementSet.LWWElementSet.getMembers')
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     def testRemoveVertex(self, mockIsMember, mockGetMembers, mockSetRemoveElement, _mockGraphRemoveVertex):
-        ''' '''
+        ''' Check remove vertex and all its edges. Also check if _removeVertex is called '''
         mockIsMember.return_value = True
         mockGetMembers.return_value = [{'a', 'b'}, {'b', 'c'}, {'c', 'a'}]
         g = LWWElementGraph()
@@ -55,7 +55,7 @@ class LWWElementGraphTests(TestCase):
 
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     def testAddEdgeFails(self, mockIsMember):
-        ''' '''
+        ''' Cannot add edge if vertex not present '''
         mockIsMember.return_value = False
         g = LWWElementGraph()
         with self.assertRaises(KeyError):
@@ -65,7 +65,7 @@ class LWWElementGraphTests(TestCase):
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     @mock.patch('LWWElementSet.LWWElementSet.addElement')
     def testAddEdge(self, mockSetAddElement, mockIsMember, _mockGraphAddEdge):
-        ''' '''
+        ''' Check if addElement to edge LWWSet. Also check if _addEdge is called '''
         mockIsMember.return_value = True
         g = LWWElementGraph()
         g.addEdge('a', 'b')
@@ -74,7 +74,7 @@ class LWWElementGraphTests(TestCase):
 
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     def testRemoveEdgeFails(self, mockIsMember):
-        ''' '''
+        ''' Cannot remove edge if it doesnt exist '''
         mockIsMember.return_value = False
         g = LWWElementGraph()
         with self.assertRaises(KeyError):
@@ -84,14 +84,14 @@ class LWWElementGraphTests(TestCase):
     @mock.patch('LWWElementSet.LWWElementSet.isMember')
     @mock.patch('LWWElementSet.LWWElementSet.removeElement')
     def testRemoveEdge(self, mockSetRemoveElement, mockIsMember, _mockGraphRemoveEdge):
-        ''' '''
+        ''' Check if removeElement in edge LWWSet. Also check if _removeEdge is called '''
         mockIsMember.return_value = True
         LWWElementGraph().removeEdge('a', 'b')
         mockSetRemoveElement.assert_called_once_with({'a','b'})
         self.assertTrue(_mockGraphRemoveEdge.called)
     
     def testIsMember(self):
-        ''' '''
+        ''' Check if isMember accesses graphState '''
         g = LWWElementGraph()
         g.graphState = {hashObj('a'): set()}
         self.assertTrue(g.isMember('a'))
@@ -99,7 +99,7 @@ class LWWElementGraphTests(TestCase):
 
     @mock.patch('LWWElementGraph.LWWElementGraph.isMember')
     def testGetNeighborsOfFails(self, mockGraphIsMember):
-        ''' '''
+        ''' Cannot getNeighboursOf if vertex not present '''
         mockGraphIsMember.return_value = False
         g = LWWElementGraph()
         with self.assertRaises(KeyError):
@@ -107,24 +107,24 @@ class LWWElementGraphTests(TestCase):
 
     @mock.patch('LWWElementGraph.LWWElementGraph.isMember')
     def testGetNeighbors(self, mockGraphIsMember):
-        ''' '''
+        ''' Check if getNeighboursOf accesses graphState '''
         mockGraphIsMember.return_value = True
         g = LWWElementGraph()
-        g.graphState = {hashObj(1): {2, 3, 4}, hashObj(2): {1}}
-        self.assertSetEqual(g.getNeighborsOf(1), {2,3,4})
-        self.assertSetEqual(g.getNeighborsOf(2), {1})
+        g.graphState = {hashObj(1): [2, 3, 4], hashObj(2): [1]}
+        self.assertListEqual(g.getNeighborsOf(1), [2,3,4])
+        self.assertListEqual(g.getNeighborsOf(2), [1])
 
     @mock.patch('LWWElementGraph.LWWElementGraph.getNeighborsOf')
     def testFindPath(self, mockGetNeighborsOf):
-        ''' '''
+        ''' Check if BFS runs correctly, given custom graph '''
         graph = {
-            1: {2}, 
-            2: {1, 3, 4},
-            3: {2, 5},
-            4: {2, 5},
-            5: {3, 4, 6, 7},
-            6: {5, 7},
-            7: {5, 6},
+            1: [2], 
+            2: [1, 3, 4],
+            3: [2, 5],
+            4: [2, 5],
+            5: [3, 4, 6, 7],
+            6: [5, 7],
+            7: [5, 6],
         }
         mockGetNeighborsOf.side_effect = lambda x: graph[x]
         g = LWWElementGraph()
@@ -132,7 +132,8 @@ class LWWElementGraphTests(TestCase):
     
     @mock.patch('LWWElementGraph.LWWElementGraph._computeGraph')
     def testMergeGraphs(self, _mockComputeGraph):
-        ''' '''
+        ''' Check if vertices and edges are merged using LLWElementSet mergeWith
+            Also check if _computeGraph is called with the right parameters '''
         g1 = LWWElementGraph()        
         g1.vertices, g1.edges = mock.MagicMock(), mock.MagicMock()
         g1.vertices.getMembers.return_value = ['v1'] 
@@ -147,29 +148,33 @@ class LWWElementGraphTests(TestCase):
     
     @mock.patch('LWWElementGraph.hashObj')
     def test_removeVertex(self, mockHash):
+        ''' Check if _removeVertex updates the graphState by removing the vertex and 
+            its edges with other vertices. '''
         mockHash.side_effect = lambda x: x
         graphState = {
-            1: {2, 4}, 
-            2: {1, 3},
-            3: {2},
-            4: {1},
+            1: [2, 4], 
+            2: [1, 3],
+            3: [2],
+            4: [1],
         }
         g = LWWElementGraph()
-        self.assertDictEqual(g._removeVertex(graphState, 2), {1: {4}, 3: set(), 4: {1}})
+        self.assertDictEqual(g._removeVertex(graphState, 2), {1: [4], 3: list(), 4: [1]})
 
     @mock.patch('LWWElementGraph.hashObj')
     def test_addEdge(self, mockHash):
+        ''' Check if _addEdge appends the vertex in the other vertex's neighbor list '''
         mockHash.side_effect = lambda x: x
-        graphState = defaultdict(set)
+        graphState = defaultdict(list)
         g = LWWElementGraph()
-        self.assertDictEqual(g._addEdge(graphState, 1, 2), {1: {2}, 2: {1}})
+        self.assertDictEqual(g._addEdge(graphState, 1, 2), {1: [2], 2: [1]})
          
     @mock.patch('LWWElementGraph.hashObj')
     def test_removeEdge(self, mockHash):
+        ''' Remove edge by deleting the vertice in the other's neighbor list '''
         mockHash.side_effect = lambda x: x
-        graphState = {1: {2}, 2: {1}}
+        graphState = {1: [2], 2: [1]}
         g = LWWElementGraph()
-        self.assertDictEqual(g._removeEdge(graphState, 1, 2), {1: set(), 2: set()})
+        self.assertDictEqual(g._removeEdge(graphState, 1, 2), {1: list(), 2: list()})
 
     @mock.patch('LWWElementGraph.hashObj')
     def test_computeGraph(self, mockHash):
@@ -191,35 +196,26 @@ class LWWElementGraphTestsComplexObject(TestCase):
     @mock.patch('LWWElementGraph.LWWElementGraph.getNeighborsOf')
     def testFindPathComplexObject(self, mockGetNeighborsOf):
         ''' '''
-
-        class ComplexClass(object):
-            def __init__(self, x, y):
-                self.x = x
-                self.y = y
-
-            def foo(self):
-                pass
-
-        a = ComplexClass(1,2)
-        b = ComplexClass(1,3)
-        c = ComplexClass(4,6)
-        d = ComplexClass(5,7)
-        e = ComplexClass(3,9)
-        f = ComplexClass(8,3)
-        h = ComplexClass(5,21)
+        a = createComplexObj()
+        b = createComplexObj()
+        c = createComplexObj()
+        d = createComplexObj()
+        e = createComplexObj()
+        f = createComplexObj()
+        h = createComplexObj()
 
         graph = {
-            hashObj(a): {b}, 
-            hashObj(b): {a, c, d},
-            hashObj(c): {b, e},
-            hashObj(d): {b, e},
-            hashObj(e): {c, d, f, h},
-            hashObj(f): {e, h},
-            hashObj(h): {e, f},
+            hashObj(a): [b], 
+            hashObj(b): [a, c, d],
+            hashObj(c): [b, e],
+            hashObj(d): [b, e],
+            hashObj(e): [c, d, f, h],
+            hashObj(f): [e, h],
+            hashObj(h): [e, f],
         }
         mockGetNeighborsOf.side_effect = lambda x: graph[hashObj(x)]
         g = LWWElementGraph()
-        self.assertListEqual(g.findPath(a, h), [a, b, d, e, h])
+        self.assertListEqual(g.findPath(a, h), [a, b, c, e, h])
 
-    # TODO: add test case in BFS for complex objects
+    # TODO: add test case for complex objects
 
