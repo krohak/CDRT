089012005db6d8739a0c94d9bd63c3759f6c7a48
krohak <rohaksinghal14@gmail.com>
2022-02-10 15:34:57 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="LWWElementGraph",
-    version="1.0.2",
+    version="1.0.3",
     description="Module for a lightweight LWWElementGraph",
     long_description=README,
     long_description_content_type="text/markdown",
